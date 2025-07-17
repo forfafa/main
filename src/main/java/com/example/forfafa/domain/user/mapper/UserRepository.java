59bad6148f936d9ed6c8ab9da2de83195705f72a
haewook Lee <atolow@gmail.com>
2025-07-17 21:44:26 +0900

    'org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.3'버전 업데이트

--- src/main/java/com/example/forfafa/domain/user/mapper/UserRepository.java
+++ src/main/java/com/example/forfafa/domain/user/mapper/UserRepository.java
@@ -1,7 +1,9 @@
 package com.example.forfafa.domain.user.mapper;
 
 import com.example.forfafa.domain.user.model.User;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface UserRepository {
     User findByUsername(String username);
     void insert(User user);
